1d1a2b36b55a193688055e9965b9c9d4f84f347a
Dominic Znakovas <znakovasdom@gmail.com>
2026-08-31 02:19:33 +0100

    Added map function that lets you scroll around and zoom

---
+++ Pace Note Generator/Frontend/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Pace_Note_Generator.Frontend.UserControls.MainMenu;
+using Pace_Note_Generator.Frontend.UserControls.MapView;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Pace_Note_Generator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var mainMenu = new MainMenu();
+
+            mainMenu.NewStageRequested += MainMenu_NewStageRequested;
+            mainMenu.OpenStageRequested += MainMenu_OpenStageRequested;
+            MainContent.Content = mainMenu;
+        }
+
+        private void MainMenu_NewStageRequested(object? sender, EventArgs e)
+        {
+            MainContent.Content = new MapView();
+        }
+
+        private void MainMenu_OpenStageRequested(object? sender, EventArgs e)
+        {
+            // will eventually load an existing stage into MapView
+        }
+    }
+} 
---
+++ Pace Note Generator/Frontend/UserControls/MainMenu/MainMenu.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Pace_Note_Generator.Frontend.UserControls.MainMenu
+{
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler? NewStageRequested;
+        public event EventHandler? OpenStageRequested;
+
+        private void BtnNewStage_Click(object sender, RoutedEventArgs e)
+        {
+            NewStageRequested?.Invoke(this, EventArgs.Empty);
+        }
+        private void BtnOpenStage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenStageRequested?.Invoke(this, EventArgs.Empty);
+        }
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
