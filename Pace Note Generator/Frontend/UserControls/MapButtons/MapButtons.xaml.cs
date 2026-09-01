250ec45cf074debdfa71370bfc77da4e5c080a69
Dominic Znakovas <znakovasdom@gmail.com>
2026-09-01 16:15:01 +0100

    Made title text have multiple colours and started on the MapButtons

---
+++ Pace Note Generator/Frontend/UserControls/MapButtons/MapButtons.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pace_Note_Generator.Frontend.UserControls.MapButtons
+{
+    /// <summary>
+    /// Interaction logic for MapButtons.xaml
+    /// </summary>
+    public partial class MapButtons : UserControl
+    {
+        public MapButtons()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddCheckpoint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
